a27bc7d6823099a753d2fa129ab7b9f2eda46e3d
Diego Leon <diegoleon.77@hotmail.com>
2022-12-15 16:29:39 -0500

    Include invoked function expression

---
+++ src/03-typing.ts
@@ -0,0 +1,13 @@
+(()=>{
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+
+  myProductName = 'Product 2';
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed(2);
+
+
+  const myProductStock = 1000;
+  const myProductName2 = 'Product';
+})();
